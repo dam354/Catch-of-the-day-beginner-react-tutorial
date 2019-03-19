a9180035b54a471e1c4537a6da84e3e6bfa7b4ee
Dan <dmennella34@gmail.com>
2019-03-18 20:43:49 -0400

    init

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// ES6 modules
+import React from 'react';
+import { render } from 'react-dom';
+import Router from './components/Router';
+import './css/style.css';
+
+render(<Router />, document.querySelector('#main'));
